892424aa4bad2c631651804b8fbc0e0d63e43467
KalabSisaySE <kyimusic11@gmail.com>
2022-12-21 23:52:16 +0300

    fix task-2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(
     () => ({ status: 200, body: 'success' }),
+  ).catch(
     () => { Error(); },
-  ).finally(() => { console.log('Got a response from the API'); });
+  ).finally(
+    () => { console.log('Got a response from the API'); },
+  );
 }
