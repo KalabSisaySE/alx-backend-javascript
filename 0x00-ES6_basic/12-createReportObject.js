f2b162835e7733713adc0839655304072803be35
KalabSisaySE <kyimusic11@gmail.com>
2022-12-24 15:59:51 +0300

    fix task-12 eslint

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,9 +1,8 @@
 export default function createReportObject(employeesList) {
-  return { 
+  return {
     allEmployees: { ...employeesList },
     getNumberOfDepartments() {
       return Object.keys(this.allEmployees).length;
-    }
+    },
   };
 }
-
