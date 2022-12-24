308267b37b619bf05a15a8c1fc8e119b3970a385
KalabSisaySE <kyimusic11@gmail.com>
2022-12-24 15:51:37 +0300

    refactor task-11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,10 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const obj = {};
-  obj[departmentName] = [];
-
-  for (const employee of employees) {
-    obj[departmentName].push(employee);
-  }
-
-  return obj;
+  return {
+    [departmentName]: employees,
+  };
 }
