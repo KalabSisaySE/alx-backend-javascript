b2a8e9b471df3672982715ada699cb8d852bca3c
KalabSisaySE <kyimusic11@gmail.com>
2022-12-27 10:12:12 +0300

    fix task-6 finally

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
-  return Promise.allSettled([promise1, promise2]).then((values) => values);
+  return Promise.allSettled([promise1, promise2]).then((values) => {
+    for (const promise of values) {
+      if (promise.status === 'rejected') {
+        promise.value = String(promise.reason);
+        delete promise.reason;
+      }
+    }
+    return values;
+  });
 }
