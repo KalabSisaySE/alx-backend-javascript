623fab51d0c3bf2500de0f39ad9b08a00b69d2d7
KalabSisaySE <kyimusic11@gmail.com>
2023-01-03 20:52:49 +0300

    fix task-8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,5 @@
 export default function cleanSet(set, startString) {
   const arr = [];
-  console.log(startString.length);
   for (const elm of set) {
     if (startString.length > 0 && elm.startsWith(startString)) {
       arr.push(elm.substring(startString.length));
