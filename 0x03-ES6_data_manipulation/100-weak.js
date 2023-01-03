5a1a84a14bdafd5a22a716f82bceea25df98f45b
KalabSisaySE <kyimusic11@gmail.com>
2023-01-03 20:44:31 +0300

    fix task-11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ let count = 1;
 function queryAPI(obj) {
   weakMap.set(obj, count);
   count += 1;
-  if (weakMap.get(obj) > 5) {
+  if (weakMap.get(obj) >= 5) {
     throw new Error('Endpoint load is high');
   }
 }
