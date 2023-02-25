a0696e0191fbe73faca25871ec92ae5476977ea9
KalabSisaySE <kyimusic11@gmail.com>
2023-02-25 21:02:03 +0300

    complete task-3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  fs.readFile(path, async (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const students = data.toString().split('\n').filter((line) => line.split(',').length === 4);
+    // remove the csv header (first element)
+    students.shift();
+    // find all the unique departments
+    const depts = new Set(students.map((student) => student.split(',')[3]));
+    console.log(`Number of students: ${students.length}`);
+    const studDept = {};
+    depts.forEach((dept) => {
+      const names = students.filter(
+        (student) => (student.split(',')[3] === dept),
+      ).map((student) => student.split(',')[0]);
+      studDept[dept] = names;
+    });
+    for (const dept in studDept) {
+      if (studDept[dept]) {
+        console.log(`Number of students in ${dept}: ${studDept[dept].length}. List: ${studDept[dept].join(', ')}`);
+      }
+    }
+  });
+}
+module.exports = countStudents;
